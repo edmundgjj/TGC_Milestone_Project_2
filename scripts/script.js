8a7b6f06e4f2fe8bb9e6a730198355396ff61523
papadum_is_nice <jimmy_hsu@singnet.com.sg>
2019-12-01 19:59:23 +0800

    Fixed Retrieve Movie Undefined

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,24 @@
-api = 'http://www.omdbapi.com/?apikey=2f191c5b&t='
-api_key = '2f191c5b'
-
-api = 'http://www.omdbapi.com/?apikey=2f191c5b&t='
-api_key = '2f191c5b'
+let api_query = 'http://www.omdbapi.com/?apikey=2f191c5b&s=';
+let api_info = 'http://www.omdbapi.com/?apikey=2f191c5b&i=';
 
 $(document).ready(function() {
     $('#searchForm').on('submit', function(e) {
-        var searchText = $('#searchText').val();
-        getMovies(searchText);
+        let searchText = $('#searchText').val();
+        retrievemovies(searchText);
         e.preventDefault();
     });
 });
 
-function getMovies(searchText) {
-    axios.get('http://www.omdbapi.com/?apikey=2f191c5b&s=' + searchText)
+function retrievemovies(movie_title) {
+    axios.get(api_query + movie_title)
         .then(function(response) {
-            console.log(response);
-            var movies = response.data.Search;
-            var output = '';
+            let movies = response.data.Search;
+            let output = '';
             $.each(movies, function(index, movie) {
                 output +=
-                    "\n <div class=\"col-md-3\">\n<div class=\"well text-center\">\n<img src=\""
+                    "\n <div class=\"col-md-3\">\n<div class=\"box text-center\">\n<img src=\""
                     .concat(movie.Poster, "\">\n<h5>")
-                    .concat(movie.Title, "</h5>\n<a onclick=\"movieSelected('")
+                    .concat(movie.Title, "</h5>\n<a onclick=\"selectMovie('")
                     .concat(movie.imdbID, "')\" class=\"btn btn-primary\" href=\"#\">Movie Details</a>\n</div>\n</div>\n");
             });
             $('#movies')
@@ -33,34 +29,70 @@ function getMovies(searchText) {
         });
 }
 
-function movieSelected(id) {
+function selectMovie(id) {
     sessionStorage.setItem('movieId', id);
     window.location = 'movie.html';
     return false;
 }
-
-function getMovie() {
-    var movieId = sessionStorage.getItem('movieId');
-    axios.get('http://www.omdbapi.com/?apikey=2f191c5b&i=' + movieId)
-        .then(function(response) {
-            console.log(response);
-            var movie = response.data;
-            var output = "\n <div class=\"row\">\n <div class=\"col-md-4\">\n <img src=\""
-                .concat(movie.Poster, "\" class=\"thumbnail\">\n </div>\n <div class=\"col-md-8\">\n <h2> ")
-                .concat(movie.Title, "</h2>\n <ul class=\"list-group\">\n <li class=\"list-group-item\"><strong>Genre:</strong>")
-                .concat(movie.Genre, "</li>\n <li class=\"list-group-item\"><strong>Released:</strong> ")
-                .concat(movie.Released, "</li>\n <li class=\"list-group-item\"><strong>Rated:</strong> ")
-                .concat(movie.Rated, "</li>\n <li class=\"list-group-item\"><strong>IMDB Rating:</strong> ")
-                .concat(movie.imdbRating, "</li>\n <li class=\"list-group-item\"><strong>Director:</strong> ")
-                .concat(movie.Director, "</li>\n <li class=\"list-group-item\"><strong>Writer:</strong> ")
-                .concat(movie.Writer, "</li>\n <li class=\"list-group-item\"><strong>Actors:</strong> ")
-                .concat(movie.Actors, "</li>\n </ul>\n </div>\n </div>\n <div class=\"row\">\n <div class=\"well\">\n <h3>Plot</h3>\n")
-                .concat(movie.Plot, "\n <hr>\n <a href=\"http://imdb.com/title/")
-                .concat(movie.imdbID, "\" target=\"_blank\" class=\"btn btn-primary\">View IMDB</a>\n <a href=\"index.html\" class=\"btn btn-default\">Go Back To Search</a>\n </div>\n </div>\n ");
-            $('#movie')
-                .html(output);
-        })
-        .catch(function(err) {
-            console.log(err);
-        });
+// test traversy
+function retrievemovie() {
+    let movieId = sessionStorage.getItem('movieId');
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', api_info + movieId, true);
+    xhr.onload = function() {
+        if (this.status === 200) {
+            const response = JSON.parse(this.responseText);
+            console.log(response)
+            let output = '';
+            if (response.Response === 'True') {
+                let movie = response;
+                output = "\n <div class=\"row\">\n <div class=\"col-md-4\">\n <img src=\""
+                    .concat(movie.Poster, "\" class=\"thumbnail\">\n </div>\n <div class=\"col-md-8\">\n <h2> ")
+                    .concat(movie.Title, "</h2>\n <ul class=\"list-group\">\n <li class=\"list-group-item\"><strong>Genre:</strong>")
+                    .concat(movie.Genre, "</li>\n <li class=\"list-group-item\"><strong>Released:</strong> ")
+                    .concat(movie.Released, "</li>\n <li class=\"list-group-item\"><strong>Rated:</strong> ")
+                    .concat(movie.Rated, "</li>\n <li class=\"list-group-item\"><strong>IMDB Rating:</strong> ")
+                    .concat(movie.imdbRating, "</li>\n <li class=\"list-group-item\"><strong>Director:</strong> ")
+                    .concat(movie.Director, "</li>\n <li class=\"list-group-item\"><strong>Writer:</strong> ")
+                    .concat(movie.Writer, "</li>\n <li class=\"list-group-item\"><strong>Actors:</strong> ")
+                    .concat(movie.Actors, "</li>\n </ul>\n </div>\n </div>\n <div class=\"row\">\n <div class=\"box\">\n <h3>Plot</h3>\n")
+                    .concat(movie.Plot, "\n <hr>\n <a href=\"http://imdb.com/title/")
+                    .concat(movie.imdbID, "\" target=\"_blank\" class=\"btn btn-primary\">View IMDB</a>\n <a href=\"index.html\" class=\"btn btn-default\">Go Back To Search</a>\n </div>\n </div>\n ");
+                // console.log(output)
+            } else {
+                output = '<li>Something went wrong</li>';
+            }
+            $('#movie').html(output);
+        }
+    }
+    xhr.send();
 }
+retrievemovie()
+
+// function retrievemovie() {
+//     let movieId = sessionStorage.getItem('movieId');
+//     axios.all([axios.get(api_info + movieId)])
+//         .then(function(response) {
+//             console.log(response);
+//             // let movie = response.data;
+//             // let output = "\n <div class=\"row\">\n <div class=\"col-md-4\">\n <img src=\""
+//             //     .concat(movie.Poster, "\" class=\"thumbnail\">\n </div>\n <div class=\"col-md-8\">\n <h2> ")
+//             //     .concat(movie.Title, "</h2>\n <ul class=\"list-group\">\n <li class=\"list-group-item\"><strong>Genre:</strong>")
+//             //     .concat(movie.Genre, "</li>\n <li class=\"list-group-item\"><strong>Released:</strong> ")
+//             //     .concat(movie.Released, "</li>\n <li class=\"list-group-item\"><strong>Rated:</strong> ")
+//             //     .concat(movie.Rated, "</li>\n <li class=\"list-group-item\"><strong>IMDB Rating:</strong> ")
+//             //     .concat(movie.imdbRating, "</li>\n <li class=\"list-group-item\"><strong>Director:</strong> ")
+//             //     .concat(movie.Director, "</li>\n <li class=\"list-group-item\"><strong>Writer:</strong> ")
+//             //     .concat(movie.Writer, "</li>\n <li class=\"list-group-item\"><strong>Actors:</strong> ")
+//             //     .concat(movie.Actors, "</li>\n </ul>\n </div>\n </div>\n <div class=\"row\">\n <div class=\"box\">\n <h3>Plot</h3>\n")
+//             //     .concat(movie.Plot, "\n <hr>\n <a href=\"http://imdb.com/title/")
+//             //     .concat(movie.imdbID, "\" target=\"_blank\" class=\"btn btn-primary\">View IMDB</a>\n <a href=\"index.html\" class=\"btn btn-default\">Go Back To Search</a>\n </div>\n </div>\n ");
+//             // $('#movie')
+//             //     .html(output);
+//         })
+//         .catch(function(err) {
+//             console.log(err);
+//         });
+// }
+
+// retrievemovie()
