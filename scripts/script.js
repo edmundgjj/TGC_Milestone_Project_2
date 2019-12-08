c66bdbc54928e11beae9fb6448e3ce1b1be36bc2
papadum_is_nice <jimmy_hsu@singnet.com.sg>
2019-12-08 17:19:51 +0800

    Movie.HTML Responsiveness Edits

--- scripts/script.js
+++ scripts/script.js
@@ -47,7 +47,7 @@ function retrievemovie() {
             if (response.Response === 'True') {
                 let movie = response;
                 output = "\n <div class=\"row\">\n <div class=\"col-md-4\">\n <img src=\""
-                    .concat(movie.Poster, "\" class=\"thumbnail\">\n </div>\n <div class=\"col-md-8\">\n <h2> ")
+                    .concat(movie.Poster, "\" id=\"poster\" class=\"thumbnail\">\n </div>\n <div class=\"col-md-8\">\n <h2> ")
                     .concat(movie.Title, "</h2>\n <ul class=\"list-group\">\n <li class=\"list-group-item\"><strong>Genre:</strong>")
                     .concat(movie.Genre, "</li>\n <li class=\"list-group-item\"><strong>Released:</strong> ")
                     .concat(movie.Released, "</li>\n <li class=\"list-group-item\"><strong>Rated:</strong> ")
@@ -55,9 +55,9 @@ function retrievemovie() {
                     .concat(movie.imdbRating, "</li>\n <li class=\"list-group-item\"><strong>Director:</strong> ")
                     .concat(movie.Director, "</li>\n <li class=\"list-group-item\"><strong>Writer:</strong> ")
                     .concat(movie.Writer, "</li>\n <li class=\"list-group-item\"><strong>Actors:</strong> ")
-                    .concat(movie.Actors, "</li>\n </ul>\n </div>\n </div>\n <div class=\"row\">\n <div class=\"box\">\n <h3>Plot</h3>\n")
+                    .concat(movie.Actors, "</li>\n </ul>\n </div>\n </div>\n <div id=\"desc\" class=\"row\">\n <div class=\"box\">\n <h3>Plot</h3>\n")
                     .concat(movie.Plot, "\n <hr>\n <a href=\"http://imdb.com/title/")
-                    .concat(movie.imdbID, "\" target=\"_blank\" class=\"btn btn-primary\">View IMDB</a>\n <a href=\"index.html\" class=\"btn btn-default\">Go Back To Search</a>\n </div>\n </div>\n ");
+                    .concat(movie.imdbID, "\" target=\"_blank\" class=\"btn btn-primary\">View IMDB</a>\n <a href=\"index.html\" class=\"btn btn-default\">Home</a>\n </div>\n </div>\n ");
                 // console.log(output)
             } else {
                 output = '<li>Something went wrong</li>';
